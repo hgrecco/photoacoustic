221037e1038082787d344b2d72954c8876f490f9
Hernan Grecco <hgrecco@gmail.com>
2025-09-05 00:15:34 -0300

    feat: expose peak_threshold_factor to user

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,6 +82,7 @@ with st.form("Source data"):
     plot_uncertainty_slope0 = st.checkbox("Plot uncertainty slope0", value=True)
     max_energy = st.number_input("Max energy", value=50.0, step=.1)
     alpha_ref = st.number_input("Reference alpha value", value=1.0, min_value=0.01, max_value=1.0, step=0.05)
+    peak_threshold_factor = st.number_input("Threshold factor", value=2.0, min_value=0.1, help="The threshold will be set to  mean(signal) + factor * std(signal)")
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("🚀 Analyze")
@@ -118,7 +119,8 @@ with st.form("Source data"):
                                 "plot_uncertainty_slope" : plot_uncertainty_slope,
                                 "plot_uncertainty_slope0" : plot_uncertainty_slope0,
                                 "max_energy" : max_energy,
-                                "alpha_ref" : alpha_ref
+                                "alpha_ref" : alpha_ref,
+                                "peak_threshold_factor": peak_threshold_factor,
                                 }
                             )
                     folder_bar.empty()

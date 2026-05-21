c9a5649e7393b9ef8297ad4c597f6afd931aa9f9
tdinapoli <tomas.dina98@gmail.com>
2026-05-21 15:51:33 +0200

    fix typo

--- src/photoacoustic/__init__.py
+++ src/photoacoustic/__init__.py
@@ -1,3 +1,3 @@
-from photacoustic import models, plotting, xlsx, input, constants, analysis
+from photoacoustic import models, plotting, xlsx, input, constants, analysis
 
 __all__ = ["models", "plotting", "xlsx", "input", "constants", "analysis"]

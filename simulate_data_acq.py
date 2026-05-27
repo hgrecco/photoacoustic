8cb0358a0d9604a37197f56d1e320dc57ed1821a
tdinapoli <tomas.dina98@gmail.com>
2026-05-27 09:58:46 +0200

    add sorting measurement files option to simulate data

--- simulate_data_acq.py
+++ simulate_data_acq.py
@@ -20,6 +20,31 @@ def initialize_watch_folder(
     return filepaths
 
 
+def sort_paths(source_path: Path, paths: list[Path]) -> list[Path]:
+    sorted_paths = []
+    ref0_paths = []
+    ref1_paths = []
+    sam_paths = []
+    for path in paths:
+        rpath = path.relative_to(source_path)
+        match rpath.parent.name[:4]:
+            case "ref0":
+                ref0_paths.append(path)
+            case "ref1":
+                ref1_paths.append(path)
+            case "sam_":
+                sam_paths.append(path)
+            case _:
+                pass
+
+    sorted_paths.append(source_path / "abs.txt")
+    sorted_paths.extend(ref0_paths)
+    sorted_paths.extend(sam_paths)
+    sorted_paths.extend(ref1_paths)
+    sorted_paths.append(source_path / "done.txt")
+    return sorted_paths
+
+
 def main():
     source_path = Path(
         "/home/tomi/Documents/academicos/doc/projects/photoacoustic/git/photoacoustic/test/data/70"
@@ -31,20 +56,22 @@ def main():
     random.shuffle(paths)
     done_path = source_path / "done.txt"
     paths.remove(done_path)
+    paths = sort_paths(source_path, paths)
+
+    paths.pop(-1)
 
     paths = initialize_watch_folder(source_path, paths, dest_path, random.randint(1, 5))
 
     input("press enter to start data copying\n")
 
     for path in paths:
-        print(path)
+        print(f"{path.parent.name=}")
         copy_path = dest_path / path.relative_to(source_path)
         print(f"coying {path} to {copy_path}")
         os.makedirs(os.path.dirname(copy_path), exist_ok=True)
         shutil.copy(path, copy_path)
 
         time.sleep(0.1)
-    shutil.copy2(done_path, dest_path)
 
 
 if __name__ == "__main__":
